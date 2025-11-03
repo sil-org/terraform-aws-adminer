50cad72e02cb74a7b57e5aeddec0cdd2af8a4d79
briskt <3172830+briskt@users.noreply.github.com>
2025-11-03 18:51:25 +0800

    remove duplicate required_providers

--- example/versions.tf
+++ example/versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_version = ">= 1.2"
   required_providers {
     aws = {
-      version = "~> 4.0"
+      version = "~> 3.0"
       source  = "hashicorp/aws"
     }
     cloudflare = {
