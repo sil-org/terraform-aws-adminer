a386f1281c18b4a88b77e005b1d07e1b894392c7
Jason Jackson <35783387+jason-jackson@users.noreply.github.com>
2026-08-25 08:30:31 -0400

    Fix awslogs-region to use current.region

--- main.tf
+++ main.tf
@@ -55,7 +55,7 @@ locals {
     logDriver : "awslogs"
     options : {
       "awslogs-group" : var.cloudwatch_log_group_name
-      "awslogs-region" : data.aws_region.current.name
+      "awslogs-region" : data.aws_region.current.region
       "awslogs-stream-prefix" : "${var.app_name}-${var.app_env}"
     }
   } : null
